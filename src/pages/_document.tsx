6f9f52a960b95d6169d51a3f3669eaa2a47b0156
Giovanni Raniolo <gianniraniolo80@gmail.com>
2025-03-26 20:01:52 +0100

    feat: update font from Italiana to SUSE Variable Font and preload for improved performance

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="it">
-      <Head />
+      <Head>
+        {/* Precarica il font SUSE Variable */}
+        <link rel="preload" href="/fonts/SUSE-VariableFont_wght.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
